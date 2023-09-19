0a56d0ddb1f5a66b974d7db3b1f6048841d4787e
Shane Dewar <ShaneB.Dewar@gmail.com>
2023-09-19 17:16:08 -0400

    feat: Winston for async logging

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 const { rateLimit } = require('express-rate-limit');
+const logger = require("./logging/logger.js");
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 10, // limit each IP to 100 requests per windowMs
     standardHeaders: false, // disable rate-limit headers
     legacyHeaders: false, // disable legacy headers
   });
@@ -26,15 +27,15 @@ const db = require("./models/database.js");
 db.mongoose
   .connect(db.url, {})
   .then(() => {
-    console.log("Connected to the database.");
+    logger.log("Connected to the database.");
   })
   .catch((err) => {
-    console.log("Cannot connect to the database.", err);
+    logger.log("Cannot connect to the database.", err);
     process.exit();
   });
 
 require("./routes/media.routes.js")(server);
 
 server.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  logger.log(`Server started on port ${port}`);
 });
